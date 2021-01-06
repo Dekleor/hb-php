b2a370e77e080193e2b1d3b5538321772a7fe6c6
Dekleor <67908404+Dekleor@users.noreply.github.com>
2021-01-06 14:13:53 +0100

    mise en page suite
    finition css header/footer + création des pages de cat

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
-
-<p>All Rights Reserved ©Bonneterie des petits pédestres</p>
+<footer>
+    <p>All Rights Reserved ©Bonneterie des petits pédestres</p>
+</footer>
 
 <script src="js/main.js"></script>
 
--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 include 'includes/header.php';
 
+echo "
+<section>
+    <h2> Qui sommes-nous ?</h2>
+    <div id='presentation'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sunt vitae ut est sapiente ab ad animi quibusdam placeat beatae! Harum unde quam asperiores aliquid aliquam iure neque architecto rem.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, voluptate necessitatibus aliquam aspernatur mollitia unde dolorum facilis dolore fugit officia quia reprehenderit veniam voluptates expedita, cum quos, nobis sequi sit.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod, expedita debitis laboriosam soluta quas magni accusamus. Dolorum, voluptas est, odio enim quia at aperiam distinctio tempore, eius quasi fuga minus.
+        </p>
+        <img src='img/presentation.jpg'>
+    </div>
+</section>";
+
 include 'includes/footer.php';
 ?>
