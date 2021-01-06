d93b7b670694c1c47f4e02e4187fef52d61a99b3
Dekleor <67908404+Dekleor@users.noreply.github.com>
2021-01-06 13:34:59 +0100

    Mise en page
    UN peu de css et rendre le truc moins dégueu

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,6 @@
 
 <p>All Rights Reserved ©Bonneterie des petits pédestres</p>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="js/main.js"></script>
 
 </body>
--- includes/header.php
+++ includes/header.php
@@ -10,18 +10,21 @@ require_once('function.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/base.css">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Bonneterie des Petits Pédestres</title>
 </head>
 <body>
 
+<header>
 <!-- une jolie image de header-->
-<h1>Les bonnets de Rémi</h1>
+    <h1><a href="index.php"> La Bonneterie des Petits Pédestres</a></h1>
 
 <!-- ici on met genre un menu de selection-->
-<ul>
-    <li><a href= "list.php">Bonnets M</a></li>
-    <li>Bonnets & Clyde</li>
-    <li>Jean Bonnets</li>
-</ul>
+<nav>
+        <a href= "homme.php">Bonnets homme</a>
+        <a href="femme.php"> Bonnets femme</a>
+        <a href="enfant.php"> Bonnets enfant</a>
+        <a href="fantasy.php"> Bonnets fantaisy</a>
+</nav>
+</header>
