07dd37320a17bda33b10a7f1fa74b67c9bfd3125
Dekleor <67908404+Dekleor@users.noreply.github.com>
2021-01-06 14:43:32 +0100

    formulaire connexion

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+    echo "<form>  
+        <div class='container'>   
+            <label>Nom d'utilisateur : </label>   
+            <input type='text' placeholder='Entrez votre nom d\'utilisateur' name='username' required>  
+            <label>Mot de passe : </label>   
+            <input type='password' placeholder='Entrez votre mot de passe' name='password' required>  
+            <button type='submit'>Se connecter</button>   
+            <input type='checkbox' checked='checked'> Se souvenir de moi   
+            <button type='button' class='cancelbtn'> Annuler</button>   
+            Forgot <a href='#'> password? </a>   
+        </div>   
+    </form>"
+
+?>
