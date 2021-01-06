2462433b4f23ea3dba8836995ccdc36f930b97d0
Dekleor <67908404+Dekleor@users.noreply.github.com>
2021-01-06 09:51:53 +0100

    create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include 'includes/header.php';
+
+echo "<div class='card' style='width: 18rem;'>
+<img src='img/P2101462.jpg' class='card-img-top' alt='bonnetA'>
+<div class='card-body'>
+    <h5 class='card-title'>Bonnet Nichon</h5>
+    <p class='card-text'>Vous en avez marre d avoir une tête de bite ?</p>
+    <a href='#' class='btn btn-primary'>Acheter MAINTENANT !</a>
+</div>
+</div>
+
+<div class='card' style='width: 18rem;'>
+<img src='img/bonnettentacule.jpg' class='card-img-top' alt='bonnetB'>
+<div class='card-body'>
+<h5 class='card-title'>Bonnet Tentacules</h5>
+<p class='card-text'>Restez au chaud tout en invoquant votre Grand Ancien préféré !</p>
+<a href='#' class='btn btn-primary'>Cthulhu R’lyeh wgah’nagl fhtagn!</a>
+</div>
+</div>
+
+<div class='card' style='width: 18rem;'>
+<img src=' img/chapeaubarbe.jpg' class='card-img-top' alt='bonnetC'>
+<div class='card-body'>
+<h5 class='card-title'>Bonnet Barbe</h5>
+<p class='card-text'>Plus un poil sur le caillou et une barbe nulle ? Une solution clef en main pour vos soucis de pilosité !</p>
+<a href='#' class='btn btn-primary'>Acheter MAINTENANT !</a>
+</div>
+</div>";
+
+include 'includes/footer.php';
+?>
